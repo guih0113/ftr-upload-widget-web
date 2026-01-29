fef6c1cc3cbb5d13e527b03c3ff700ecd665b33a
guih0113 <gui.henriquess13@gmail.com>
2026-01-29 15:41:04 -0300

    feat: gradient border on the button

--- src/components/upload-widget.tsx
+++ src/components/upload-widget.tsx
@@ -6,12 +6,15 @@ import { UploadWidgetMinimizedButton } from './upload-widget-minimized-button'
 import { UploadWidgetUploadList } from './upload-widget-upload-list'
 
 export function UploadWidget() {
+  const isThereAnyPendingUpload = true
+
   const [isWidgetOpen, toggleWidgetOpen] = useCycle(false, true)
 
   return (
-    <Collapsible.Root onOpenChange={() => toggleWidgetOpen()}>
+    <Collapsible.Root onOpenChange={() => toggleWidgetOpen()} asChild>
       <motion.div
-        className="w-90 overflow-hidden rounded-xl bg-zinc-900 shadow-shape"
+        data-progress={isThereAnyPendingUpload}
+        className="w-90 animate-border overflow-hidden rounded-xl border border-transparent bg-zinc-900 data-[state=closed]:data-[progress=false]:shadow-shape data-[state=closed]:rounded-3xl data-[state=open]:shadow-shape data-[state=closed]:data-[progress=true]:[background:linear-gradient(45deg,#09090B,--theme(--color-zinc-900)_50%,#09090B)_padding-box,conic-gradient(from_var(--border-angle),--theme(--color-zinc-700/.48)_80%,--theme(--color-indigo-500)_86%,--theme(--color-indigo-300)_90%,--theme(--color-indigo-500)_94%,--theme(--color-zinc-600/.48))_border-box]"
         animate={isWidgetOpen ? 'open' : 'closed'}
         variants={{
           closed: {
